550f04d4fc938ef9084dce4d5c1e001dac0afaa0
krzychek <krzysiek1993r@gmail.com>
2015-01-12 11:54:57 +0100

    * gui form

---
+++ to_shop/gui/MainWindow.java
@@ -0,0 +1,17 @@
+package to_shop.gui;
+
+import javax.swing.*;
+
+public class MainWindow {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTree tree1;
+    private JTree tree2;
+    private JButton button1;
+    private JButton button2;
+    private JButton kupButton;
+    private JTextField textField1;
+    private JTree tree3;
+    private JTree tree4;
+    private JList list1;
+}
