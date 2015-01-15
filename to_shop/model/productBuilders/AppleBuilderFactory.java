021ef983bdaa3a2e3b3a4451aa679520ff49490a
krzychek <krzysiek1993r@gmail.com>
2015-01-15 22:33:41 +0100

    comment

--- to_shop/model/productBuilders/AppleBuilderFactory.java
+++ to_shop/model/productBuilders/AppleBuilderFactory.java
@@ -2,7 +2,7 @@ package to_shop.model.productBuilders;
 
 import to_shop.model.products.Apple;
 
-public class AppleBuilderFactory {
+public class AppleBuilderFactory { // TODO change to factory methdo, use!!
     public AppleBuilder getAppleBuilder(Apple.AppleType type) {
         switch (type) {
             case BRAMEY: return new BramleyAppleBuilder();
